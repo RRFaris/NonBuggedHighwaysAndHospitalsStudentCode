a1f7538d103ba976f2dada2212b53992c53b5513
Ryan-dev12 <ryan.faris007@gmail.com>
2025-09-22 00:36:53 -0700

    Messed everything up when trying to include weight balancing, so I finally got everything back to normal (Path compression).

--- src/HighwaysAndHospitals.java
+++ src/HighwaysAndHospitals.java
@@ -4,17 +4,56 @@
  * for Adventures in Algorithms
  * at Menlo School in Atherton, CA
  *
- * Completed by: [YOUR NAME HERE]
+ * Completed by: Ryan Faris
  *
  */
 
 public class HighwaysAndHospitals {
-
-    /**
-     * TODO: Complete this function, cost(), to return the minimum cost to provide
-     *  hospital access for all citizens in Menlo County.
-     */
     public static long cost(int n, int hospitalCost, int highwayCost, int cities[][]) {
-        return 0;
+        if (hospitalCost < highwayCost) {
+            return ((long) n * hospitalCost);
+        }
+
+        int[] roots = new int[n + 1];
+
+        for (int i = 0; i < cities.length; i++) {
+            int a = cities[i][0];
+            int b = cities[i][1];
+
+            int rootA = a;
+            while (roots[rootA] != 0) {
+                rootA = roots[rootA];
+            }
+
+            while (roots[a] != 0) {
+                int temp = roots[a];
+                roots[a] = rootA;
+                a = temp;
+            }
+
+            int rootB = b;
+            while (roots[rootB] != 0) {
+                rootB = roots[rootB];
+            }
+
+            while (roots[b] != 0) {
+                int temp = roots[b];
+                roots[b] = rootB;
+                b = temp;
+            }
+
+            if (rootA != rootB) {
+                roots[rootB] = rootA;
+            }
+        }
+
+        int num_subtrees = 0;
+        for (int i = 1; i <= n; i++) {
+            if (roots[i] == 0) {
+                num_subtrees++;
+            }
+        }
+
+        return (long) num_subtrees * hospitalCost + (long) (n - num_subtrees) * highwayCost;
     }
 }
